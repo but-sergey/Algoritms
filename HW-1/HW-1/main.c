a6013e79f0cfc1bd1c94775c68251942c24f39ac
but-sergey <but-sergey@bk.ru>
2020-01-14 22:25:51 +0300

    Задачи 01 - 09

--- HW-1/HW-1/main.c
+++ HW-1/HW-1/main.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 #include<Windows.h>
 #pragma warning(disable:6031)
 
@@ -24,12 +25,18 @@ void task14();
 
 int main(int argc, const char* argv[])
 {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
 	int sel = 0;
 	do
 	{
 		menu();
 		scanf("%i", &sel);
-		system("cls");
+		//sel = 6;
+
+		if (sel != 0) { system("cls"); }
+
 		switch (sel)
 		{
 		case 1: task01();
@@ -69,27 +76,27 @@ int main(int argc, const char* argv[])
 void menu()
 {
 	system("cls");
-	printf("1 - task01\n");
-	printf("2 - task02\n");
-	printf("3 - task03\n");
-	printf("4 - task04\n");
-	printf("5 - task05\n");
-	printf("6 - task06\n");
-	printf("7 - task07\n");
-	printf("8 - task08\n");
-	printf("9 - task09\n");
+	printf("1 - Задача 01 (ИМТ)\n");
+	printf("2 - Задача 02 (максимальное из 4 чисел)\n");
+	printf("3 - Задача 03 (обмен значениями 2 переменных)\n");
+	printf("4 - Задача 04 (квадратное уравнение)\n");
+	printf("5 - Задача 05 (месяц и времена года)\n");
+	printf("6 - Задача 06 (возраст)\n");
+	printf("7 - Задача 07 (клетки на шахматной доске)\n");
+	printf("8 - Задача 08 (квадраты и кубы чисел)\n");
+	printf("9 - Задача 09 (деление вычитанием)\n");
 	printf("10 - task10\n");
 	printf("11 - task11\n");
 	printf("12 - task12\n");
 	printf("13 - task13\n");
 	printf("14 - task14\n");
 	printf("\n");
-	printf("0 - exit\n");
+	printf("0 - выход\n");
 }
 
 void pause()
 {
-	printf("Press <Enter> to continue...");
+	printf("Нажмите <Enter> для продолжения...");
 	getch();
 }
 
@@ -98,7 +105,20 @@ void pause()
 //
 void task01()
 {
-	printf("Task 01\n");
+	printf("Задача 01 (ИМТ)\n\n");
+
+	double m;
+	double h;
+	double I;
+
+	printf("Масса, кг: ");
+	scanf("%lf", &m);
+
+	printf("Рост, м: ");
+	scanf("%lf", &h);
+
+	I = m / (h * h);
+	printf("ИМТ = %.2lf\n", I);
 
 	pause();
 }
@@ -107,7 +127,28 @@ void task01()
 //
 void task02()
 {
-	printf("Task 02\n");
+	printf("Задача 02 (максимальное из 4 чисел)\n");
+
+	int x1, x2, x3, x4;
+	int max, max1, max2;
+	
+	printf("Число №1: ");
+	scanf("%d", &x1);
+
+	printf("Число №2: ");
+	scanf("%d", &x2);
+
+	printf("Число №3: ");
+	scanf("%d", &x3);
+
+	printf("Число №4: ");
+	scanf("%d", &x4);
+
+	max1 = (x1 > x2) ? x1 : x2;
+	max2 = (x3 > x4) ? x3 : x4;
+	max = (max1 > max2) ? max1 : max2;
+
+	printf("Максимальное число = %d\n", max);
 
 	pause();
 }
@@ -118,7 +159,29 @@ void task02()
 //
 void task03()
 {
-	printf("Task 03\n");
+	printf("Задача 03 (обмен значениями 2 переменных)\n");
+
+	int a = 1;
+	int b = 2;
+	int t;
+
+	printf("a = %d, b = %d\n", a, b);
+	t = a;
+	a = b;
+	b = t;
+	printf("a = %d, b = %d\n", a, b);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("a = %d, b = %d\n", a, b);
+	a = a * b;
+	b = a / b;
+	a = a / b;
+	printf("a = %d, b = %d\n", a, b);
+	a = a ^ b;
+	b = b ^ a;
+	a = a ^ b;
+	printf("a = %d, b = %d\n", a, b);
 
 	pause();
 }
@@ -127,7 +190,51 @@ void task03()
 //
 void task04()
 {
-	printf("Task 04\n");
+	printf("Задача 04 (квадратное уравнение)\n");
+	double a, b, c;
+	double D, x1, x2;
+
+	printf("a*x^2 + b*x + c = 0\n");
+	
+	printf("a = ");
+	scanf("%lf", &a);
+	printf("b = ");
+	scanf("%lf", &b);
+	printf("c = ");
+	scanf("%lf", &c);
+
+	if (a == 0)
+	{
+		if (b == 0)
+		{
+			printf("Это не уравнение, a или b не должно равняться 0.\n");
+		}
+		else
+		{
+			x1 = -c / b;
+			printf("Линейное уравнение, x = %.2lf.\n", x1);
+		}
+	}
+	else
+	{
+		D = b * b - 4 * a * c;
+		printf("D = %.2lf\n", D);
+		if (D == 0)
+		{
+			x1 = -b / (2 * a);
+			printf("Два одинаковых корня, x1 = x2 = %.2lf.\n", x1);
+		}
+		else if (D > 0)
+		{
+			x1 = (-b + sqrt(D)) / (2 * a);
+			x2 = (-b - sqrt(D)) / (2 * a);
+			printf("Два действительных корня: x1 = %.2lf и x2 = %.2lf.\n", x1, x2);
+		}
+		else
+		{	// здесь можно было бы ещё расписать комплексные корни, но лень... )))
+			printf("На множестве действительных чисел уравнение корней не имеет.\n");
+		}
+	}
 
 	pause();
 }
@@ -137,8 +244,35 @@ void task04()
 //
 void task05()
 {
-	printf("Task 05\n");
+	printf("Задача 05 (месяц и времена года)\n");
 
+	int month;
+
+	printf("Номер месяца: ");
+	scanf("%d", &month);
+	
+	switch (month)
+	{
+	case 12:
+	case 1:
+	case 2: printf("Это зима.\n");
+		break;
+	case 3:
+	case 4:
+	case 5: printf("Это весна.\n");
+		break;
+	case 6:
+	case 7:
+	case 8: printf("Это лето.\n");
+		break;
+	case 9:
+	case 10:
+	case 11: printf("Это зима.\n");
+		break;
+	default:
+		printf("Неверный номер месяца.\n");
+	}
+	
 	pause();
 }
 
@@ -147,7 +281,36 @@ void task05()
 //
 void task06()
 {
-	printf("Task 06\n");
+	printf("Задача 06 (возраст)\n");
+	
+	int age;
+
+	printf("Ваш возраст: ");
+	scanf("%d", &age);
+	if ((age >= 1) && (age <= 150))
+	{
+		if ((age % 100 >= 11) && (age % 100 <= 14))
+		{
+			printf("Вам %d лет.\n", age);
+		}
+		else
+		{
+			switch (age % 10)
+			{
+			case 1: printf("Вам %d год.\n", age);
+				break;
+			case 2:
+			case 3:
+			case 4: printf("Вам %d года.\n", age);
+				break;
+			default: printf("Вам %d лет.\n", age);
+			}
+		}
+	}
+	else
+	{
+		printf("Неверный возраст. Возраст должен быть от 1 до 150 включительно.\n");
+	}
 
 	pause();
 }
@@ -158,7 +321,27 @@ void task06()
 //
 void task07()
 {
-	printf("Task 07\n");
+	printf("Задача 07 (клетки на шахматной доске)\n");
+
+	int x1, y1, x2, y2;
+	
+	printf("Координаты клетки №1, x1-y1: ");
+	scanf("%d-%d", &x1, &y1);
+	printf("Координаты клетки №2, x2-y2: ");
+	scanf("%d-%d", &x2, &y2);
+
+	printf("\nДалее исходим из условия, что левая нижняя клетка (1-1) белого цвета.\n\n");
+
+	if ((x1 + y2) % 2 == (x2 + y2) % 2)
+	{
+		printf("Клетки одного цвета: %s.\n", ((x1 + y1) % 2 == 0) ? "белого" : "черного");
+	}
+	else
+	{
+		printf("Клетки разного цвета. Клетка №1 %s, а клетка №2 %s.\n",
+					((x1 + y1) % 2 == 0) ? "белая" : "черная",
+					((x2 + y2) % 2 == 0) ? "белая" : "черная");
+	}
 
 	pause();
 }
@@ -167,7 +350,27 @@ void task07()
 //
 void task08()
 {
-	printf("Task 08\n");
+	printf("Задача 08 (квадраты и кубы чисел)\n");
+
+	int a, b;
+
+	printf("Число a: ");
+	scanf("%d", &a);
+	printf("Число b: ");
+	scanf("%d", &b);
+	if (a <= b)
+	{
+		printf("    x       x^2       x^3\n\n");
+		do
+		{
+			printf("%5d%10d%10d\n", a, a * a, a * a * a);
+			a++;
+		} while (a <= b);
+	}
+	else
+	{
+		printf("Число a не должно быть больше, чем число b");
+	}
 
 	pause();
 }
@@ -178,7 +381,25 @@ void task08()
 //
 void task09()
 {
-	printf("Task 09\n");
+	printf("Задача 09 (деление вычитанием)\n");
+
+	int N, K;
+	int Q = 0;
+	int R = 0;
+
+	printf("Делимое N: ");
+	scanf("%d", &N);
+	printf("Делитель K: ");
+	scanf("%d", &K);
+
+	while (N >= K)
+	{
+		N -= K;
+		Q++;
+	}
+	R = N;
+
+	printf("\nЧастное Q = %d, остаток R = %d\n", Q, R);
 
 	pause();
 }
