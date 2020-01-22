84fbe9f3371be5b4a8ab8ec50988b7bd26b9d271
but-sergey <but-sergey@bk.ru>
2020-01-22 16:18:15 +0300

    Задачи 3 и 4 решены

--- HW-3/HW-3/main.c
+++ HW-3/HW-3/main.c
@@ -75,12 +75,27 @@ void swap(int *a, int *b)
 
 void print(int N, int* a)
 {
-	printf("\n");
 	for (int i = 0; i < N; i++)
 	{
 		printf("%4d", a[i]);
 	}
 	printf("\n");
+}
+
+void fillRandom(int N, int* a)
+{
+	for (int i = 0; i < N; i++)
+	{
+		a[i] = rand() % MaxN;
+	}
+}
+
+void copyArray(int N, int* a, int* b)
+{
+	for (int i = 0; i < N; i++)
+	{
+		b[i] = a[i];
+	}
 }
 
 // 1. Попробовать оптимизировать пузырьковую сортировку. Сравнить количество операций
@@ -89,16 +104,16 @@ void print(int N, int* a)
 //
 void Sort1(int N, int* a, int* c_if, int* c_swap)
 {
-	*c_if = 0;
-	*c_swap = 0;
+	if (c_if != NULL) { *c_if = 0; }
+	if (c_swap != NULL) { *c_swap = 0; }
 
 	for(int i = 0; i < N; i++)
 		for (int j = 0; j < N - 1; j++)
 		{
-			*c_if += 1;
+			if (c_if != NULL) { *c_if += 1; }
 			if (a[j] > a[j + 1])
 			{
-				*c_swap += 1;
+				if (c_swap != NULL) { *c_swap += 1; }
 				swap(&a[j], &a[j + 1]);
 			}
 		}
@@ -107,17 +122,17 @@ void Sort1(int N, int* a, int* c_if, int* c_swap)
 
 int Sort2(int N, int* a, int* c_if, int* c_swap)
 {
-	*c_if = 0;
-	*c_swap = 0;
+	if (c_if != NULL) { *c_if = 0; }
+	if (c_swap != NULL) { *c_swap = 0; }
 
 	for (int i = 0; i < N; i++)
 		// нет смысла просматривать уже отсортированную область, её исключаем
 		for (int j = 0; j < N - i - 1; j++)
 		{
-			*c_if += 1;
+			if (c_if != NULL) { *c_if += 1; }
 			if (a[j] > a[j + 1])
 			{
-				*c_swap += 1;
+				if (c_swap != NULL) { *c_swap += 1; }
 				swap(&a[j], &a[j + 1]);
 			}
 		}
@@ -134,21 +149,18 @@ void task01()
 	int c_swap;
 	
 	// заполнение двух одинаковых массивов для сравнения алгоритмов
-	for (int i = 0; i < MaxN; i++)
-	{
-		a[i] = rand() % 100;
-		b[i] = a[i];
-	}
+	fillRandom(MaxN, &a);
+	copyArray(MaxN, &a, &b);
 
-	printf("Массив до сортировки: ");
+	printf("Массив до сортировки:\n");
 	print(MaxN, a);
 
 	Sort1(MaxN, a, &c_if, &c_swap);
-	printf("Массив после обычной сортировки (%d операций сравнения, %d обменов): ", c_if, c_swap);
+	printf("Массив после обычной сортировки (%d операций сравнения, %d обменов):\n", c_if, c_swap);
 	print(MaxN, a);
 
 	Sort2(MaxN, b, &c_if, &c_swap);
-	printf("Массив после оптимизированной сортировки (%d операций сравнения, %d обменов): ", c_if, c_swap);
+	printf("Массив после оптимизированной сортировки (%d операций сравнения, %d обменов):\n", c_if, c_swap);
 	print(MaxN, b);
 	
 	pause();
@@ -160,28 +172,28 @@ void task01()
 
 void Sort3(int N, int* a, int* c_if, int* c_swap)
 {
-	*c_if = 0;
-	*c_swap = 0;
+	if (c_if != NULL) { *c_if = 0; }
+	if (c_swap != NULL) { *c_swap = 0; }
 
 	for (int i = 0; i < (N + 1) / 2; i++)
 	{
 		// проход "туда"
 		for (int j = i; j < N - i - 1; j++)
 		{
-			*c_if += 1;
+			if (c_if != NULL) { *c_if += 1; }
 			if (a[j] > a[j + 1])
 			{
-				*c_swap += 1;
+				if (c_swap != NULL) { *c_swap += 1; }
 				swap(&a[j], &a[j + 1]);
 			}
 		}
 		// проход "обратно"
 		for (int j = N - i - 3; j >= i; j--)
 		{
-			*c_if += 1;
+			if (c_if != NULL) { *c_if += 1; }
 			if (a[j] > a[j + 1])
 			{
-				*c_swap += 1;
+				if (c_swap != NULL) { *c_swap += 1; }
 				swap(&a[j], &a[j + 1]);
 			}
 		}
@@ -199,21 +211,18 @@ void task02()
 	int c_swap;
 
 	// заполнение двух одинаковых массивов для сравнения алгоритмов
-	for (int i = 0; i < MaxN; i++)
-	{
-		a[i] = rand() % 100;
-		b[i] = a[i];
-	}
+	fillRandom(MaxN, &a);
+	copyArray(MaxN, &a, &b);
 
-	//printf("Массив до сортировки: ");
-	//print(MaxN, a);
+	printf("Массив до сортировки:\n");
+	print(MaxN, a);
 
 	Sort2(MaxN, a, &c_if, &c_swap);
-	printf("Массив после оптимизированной пузырьковой сортировки\n(%d операций сравнения, %d обменов, %d всего): ", c_if, c_swap, c_if + c_swap);
+	printf("Массив после оптимизированной пузырьковой сортировки\n(%d операций сравнения, %d обменов, %d всего):\n", c_if, c_swap, c_if + c_swap);
 	print(MaxN, a);
 
 	Sort3(MaxN, b, &c_if, &c_swap);
-	printf("Массив после шейкерной сортировки\n(%d операций сравнения, %d обменов, %d всего): ", c_if, c_swap, c_if + c_swap);
+	printf("Массив после шейкерной сортировки\n(%d операций сравнения, %d обменов, %d всего):\n", c_if, c_swap, c_if + c_swap);
 	print(MaxN, b);
 	
 	pause();
@@ -222,9 +231,72 @@ void task02()
 // 3. Реализовать бинарный алгоритм поиска в виде функции, которой передается отсортированный
 // массив. Функция возвращает индекс найденного элемента или - 1, если элемент не найден.
 //
+
+int findBin(int N, int* a, int num)
+{
+	int result = -1;
+	int min = 0;
+	int max = N - 1;
+	int ind = min + (max - min) / 2;
+
+	while (min!=max)
+	{
+		ind = min + (max - min) / 2;
+		if (a[ind] == num)
+		{
+			result = ind;
+			break;
+		}
+		else if (a[ind] > num)
+		{
+			max = ind - 1;
+		}
+		else
+		{
+			min = ind + 1;
+		}
+	}
+	if (a[min] == num)
+	{
+		result = min;
+	}
+
+
+	return result;
+}
+
 void task03()
 {
 	printf("Задача 03 (бинарный поиск)\n\n");
+
+	int a[MaxN];
+	int num;
+	int ind;
+	
+	// заполнение массива случайными значениями
+	fillRandom(MaxN, &a);
+
+	// сортировка (подсчет шагов не нужен)
+	Sort3(MaxN, &a, NULL, NULL);
+
+	printf("Исходный массив:\n");
+	print(MaxN, &a);
+
+	// ввод элемента для поиска
+	printf("Элемент для поиска: ");
+	scanf("%d", &num);
+
+	// поиск
+	ind = findBin(MaxN, &a, num);
+
+	if (ind == -1)
+	{
+		printf("Искомый элемент %d не найден.\n", num);
+	}
+	else
+	{
+		printf("Искомый элемент %d имеет индекс %d.\n", num, ind);
+	}
 
 	pause();
 }
@@ -234,7 +306,27 @@ void task03()
 //
 void task04()
 {
+	int a[MaxN];
+	int b[MaxN];
+	int c[MaxN];
+	int c_if;
+	int c_swap;
+
+	fillRandom(MaxN, &a);
+	copyArray(MaxN, &a, &b);
+	copyArray(MaxN, &a, &c);
+
 	printf("Задача 04 (расчет сложности алгоритма)\n\n");
+
+	printf("Асимптотическая сложность пузырьковой сортировки O(N^2)\n");
+	printf("Для N=%d сложность O(N^2) = %d\n\n", MaxN, MaxN*MaxN);
+	printf("В нашем случае количество операций:\n");
+	Sort1(MaxN, &a, &c_if, &c_swap);
+	printf("Классический алгоритм: %d\n", c_if + c_swap);
+	Sort2(MaxN, &b, &c_if, &c_swap);
+	printf("Оптимизированный алгоритм: %d\n", c_if + c_swap);
+	Sort3(MaxN, &c, &c_if, &c_swap);
+	printf("Шейкерная сортировка: %d\n", c_if + c_swap);
 
 	pause();
 }
