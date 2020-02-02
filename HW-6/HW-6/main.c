98aa0d7827add34a8e06256409467c5281661090
but-sergey <but-sergey@bk.ru>
2020-02-02 21:09:52 +0300

    Задачи 1 и 2 решены

--- HW-6/HW-6/main.c
+++ HW-6/HW-6/main.c
@@ -9,6 +9,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<malloc.h>
 #include<Windows.h>
 #pragma warning(disable:6031)
 
@@ -49,7 +50,7 @@ void menu()
 	system("cls");
 	printf(" 1 - Задача 01 (хэш-функция)\n");
 	printf(" 2 - Задача 02 (двоичное дерево поиска)\n");
-	printf(" 3 - Задача 03 (база данных студентов)\n\n");
+	//printf(" 3 - Задача 03 (база данных студентов)\n\n");
 	printf(" 0 - выход\n\n");
 	printf(" -=> ");
 }
@@ -95,9 +96,162 @@ void task01()
 //   в)* Добавить в программу обработку командной строки с помощью которой можно указывать
 //   из какого файла считывать данные, каким образом обходить дерево.
 //
+typedef int T;
+typedef struct Node
+{
+	T data;
+	struct Node* left;
+	struct Node* right;
+	struct Node* parent;
+} Node;
+
+// Распечатка двоичного дерева в виде скобочной записи
+void printTree(Node* root)
+{
+	if (root)
+	{
+		printf("%d", root->data);
+		if (root->left || root->right)
+		{
+			printf("(");
+			if (root->left)
+				printTree(root->left);
+			else
+				printf("NULL");
+			printf(",");
+
+			if (root->right)
+				printTree(root->right);
+			else
+				printf("NULL");
+			printf(")");
+		}
+	}
+}
+
+// Создание нового узла
+Node* getFreeNode(T value, Node* parent)
+{
+	Node* tmp = (Node*)malloc(sizeof(Node));
+	tmp->left = NULL;
+	tmp->right = NULL;
+	tmp->data = value;
+	tmp->parent = parent;
+	return tmp;
+}
+
+// Вставка узла
+void insert(Node** head, int value)
+{
+	Node* tmp = NULL;
+	if (*head == NULL)
+	{
+		*head = getFreeNode(value, NULL);
+		return;
+	}
+
+	tmp = *head;
+	while (tmp)
+	{
+		if (value > tmp->data)
+		{
+			if (tmp->right)
+			{
+				tmp = tmp->right;
+				continue;
+			}
+			else
+			{
+				tmp->right = getFreeNode(value, tmp);
+				return;
+			}
+		}
+		else if (value < tmp->data)
+		{
+			if (tmp->left)
+			{
+				tmp = tmp->left;
+				continue;
+			}
+			else
+			{
+				tmp->left = getFreeNode(value, tmp);
+				return;
+			}
+		}
+		else
+		{
+			printf("Дерево построено неправильно");
+			return;
+		}
+	}
+}
+
+void preOrderTravers(Node* root)
+{
+	if (root)
+	{
+		printf("%d ", root->data);
+		preOrderTravers(root->left);
+		preOrderTravers(root->right);
+	}
+}
+
+void inOrderTravers(Node* root)
+{
+	if (root)
+	{
+		inOrderTravers(root->left);
+		printf("%d ", root->data);
+		inOrderTravers(root->right);
+	}
+}
+
+void postOrderTravers(Node* root)
+{
+	if (root)
+	{
+		postOrderTravers(root->left);
+		postOrderTravers(root->right);
+		printf("%d ", root->data);
+	}
+}
+
 void task02()
 {
 	printf("Задача 02 (двоичное дерево поиска)\n\n");
+
+	Node* Tree = NULL;
+	char* fileName = "..\\data.txt";
+	FILE* file = fopen(fileName, "r");
+	if (file == NULL)
+	{
+		printf("Невозможно открыть файл!\n");
+		pause();
+		return;
+	}
+	int count;
+	fscanf(file, "%d", &count);
+	for (int i = 0; i < count; i++)
+	{
+		int value;
+		fscanf(file, "%d", &value);
+		insert(&Tree, value);
+	}
+	fclose(file);
+	printTree(Tree);
+	printf("\n");
+	
+	printf("\nPreOrderTravers:  ");
+	preOrderTravers(Tree);
+	
+	printf("\nInOrderTravers:   ");
+	inOrderTravers(Tree);
+	
+	printf("\nPostOrderTravers: ");
+	postOrderTravers(Tree);
+	
+	printf("\n");
 
 	pause();
 }
