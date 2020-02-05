0a7db261c20af00b078556aa0f63cada6ded287a
but-sergey <but-sergey@bk.ru>
2020-02-05 22:10:20 +0300

    Задача 1 решена

--- HW-7/HW-7/main.c
+++ HW-7/HW-7/main.c
@@ -39,8 +39,8 @@ int main(int argc, const char* argv[])
 			break;
 		case 3: task03();
 			break;
-		case 4: task04();
-			break;
+//		case 4: task04();
+//			break;
 		}
 	} while (sel != 0);
 
@@ -53,7 +53,7 @@ void menu()
 	printf(" 1 - Задача 01 (матрица смежности)\n");
 	printf(" 2 - Задача 02 (обход графа в глубину)\n");
 	printf(" 3 - Задача 03 (обход графа в ширину)\n");
-	printf(" 3 - Задача 04 (библиотека функций для работы с графами)\n\n");
+	//printf(" 4 - Задача 04 (библиотека функций для работы с графами)\n\n");
 	printf(" 0 - выход\n\n");
 	printf(" -=> ");
 }
@@ -66,18 +66,91 @@ void pause()
 
 // 1. Написать функции, которые считывают матрицу смежности из файла и выводят ее на экран
 //
+
+int** ReadMatrix(char* fileName, int* N)
+{
+	// объявление переменной файла
+	FILE* file = fopen(fileName, "r");
+	if (file == NULL)
+	{
+		printf("Файл не найден!\n");
+		return NULL;
+	}
+	
+	// чтение из файла размера матрицы
+	fscanf(file, "%d", N);
+	
+	// резервирование памяти под матрицу
+	int** Matrix = (int**)malloc(*N * sizeof(int*));
+	for (int i = 0; i < *N; i++)
+	{
+		Matrix[i] = (int*)malloc(*N * sizeof(int));
+	}
+
+	// чтение матрицы из файла
+	for (int i = 0; i < *N; i++)
+	{
+		for (int j = 0; j < *N; j++)
+		{
+			fscanf(file, "%d", &Matrix[i][j]);
+		}
+	}
+
+	return Matrix;
+}
+
+void PrintMatrix(int** Matrix, int N)
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			printf("%4d ", Matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void FreeMatrix(int** Matrix, int N)
+{
+	for (int i = 0; i < N; i++)
+	{
+		free(Matrix[i]);
+	}
+	free(Matrix);
+	Matrix = NULL;
+}
+
 void task01()
 {
 	printf("Задача 01 (матрица смежности)\n\n");
+
+	int N;
+	int** Matrix = ReadMatrix("..\\matrix.txt", &N);
+
+	PrintMatrix(Matrix, N);
+
+	FreeMatrix(Matrix, N);
 
 	pause();
 }
 
 // 2. Написать рекурсивную функцию обхода графа в глубину.
+// https://shpargalum.ru/shpora-gos-povtas/strukturyi-i-algoritmyi-obrabotki-dannyix/grafyi.-obxod-grafov-v-shirinu-i-glubinu.html
 //
 void task02()
 {
 	printf("Задача 02 (обход графа в глубину)\n\n");
+
+	// чтение графа
+	int N;
+	int** Matrix = ReadMatrix("..\\matrix.txt", &N);
+
+	PrintMatrix(Matrix, N);
+
+
+	// освобождение памяти
+	FreeMatrix(Matrix, N);
 
 	pause();
 }
@@ -87,6 +160,17 @@ void task02()
 void task03()
 {
 	printf("Задача 03 (обход графа в ширину)\n\n");
+
+	// чтение графа
+	int N;
+	int** Matrix = ReadMatrix("..\\matrix.txt", &N);
+
+	PrintMatrix(Matrix, N);
+
+
+
+	// освобождение памяти
+	FreeMatrix(Matrix, N);
 
 	pause();
 }
